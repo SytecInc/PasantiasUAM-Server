e2b15ac5c6504da5be1e564fb7f67f5389af07ce
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-11-17 08:39:48 -0500

    Companies CRUD implementation

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -3,6 +3,10 @@ const CompanyController = require("../app/Controllers/company.controller");
 
 const api = express.Router();
 
+api.get("/companies", CompanyController.index);
+api.get("/companies/:id", CompanyController.show);
 api.post("/companies", CompanyController.store);
+api.put("/companies/:id", CompanyController.update);
+api.delete("/companies/:id", CompanyController.destroy);
 
 module.exports = api;
